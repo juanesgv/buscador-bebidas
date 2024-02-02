520e47f29232d87c716edf81d67998eddaa65235
juanesgv <juanesgarviedma23@gmail.com>
2024-02-02 10:05:44 -0500

    Se inicia el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Container } from "react-bootstrap"
+
+function App() {
+
+  return (
+    <>
+      <header className="py-5">
+        <h1>Bucador de bebidas</h1>
+      </header>
+
+      <Container className="mt-5">
+        1
+      </Container>
+    </>
+  )
+}
+
+export default App

a067b2c8902f81a2b28d945fa2d3c8d1a00775eb
juanesgv <juanesgarviedma23@gmail.com>
2024-02-02 17:30:34 -0500

    Se muestran el listado de bebidas

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Col, Card, Button } from 'react-bootstrap'
 
 const Bebida = ({bebida}) => {
   return (
-    <div>
-      {bebida.strDrink}
-    </div>
+    <Col md={6} lg={3}>
+        <Card className='mb-4'>
+            <Card.Img
+                variant='top'
+                src={bebida.strDrinkThumb}
+                alt= {`Imagen de ${bebida.strDrink}`}
+            />
+            <Card.Body>
+                <Card.Title>{bebida.strDrink}</Card.Title>
+                <Button variant="secondary" className='w-100 text-uppercase mt-2'>
+                    Ver receta
+                </Button>
+            </Card.Body>
+        </Card>    
+    </Col>
   )
 }
 
